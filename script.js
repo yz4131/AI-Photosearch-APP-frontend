b73a23417405dc3d309119893463cc961a4785e9
Yuqin Zhao <yz4131@columbia.edu>
2022-03-24 23:02:54 -0400

    front

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const image_input = document.querySelector("#image_input");
 const label_input = document.querySelector("#fname");
-const btn = document.querySelector("#btn");
-var uploaded_image = "";
+var labels = document.getElementById("fname").value;  
+
+
 
 image_input.addEventListener("change", function(){
     const reader = new FileReader();
@@ -9,19 +10,22 @@ image_input.addEventListener("change", function(){
         uploaded_image = reader.result;
         document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`
     });
-    reader.readAsDataURL(this.files[0])
-})
-
-btn.addEventListener("click", function(){
-    fetch('http://www.example.net',{
+    reader.readAsDataURL(this.files[0]);
+    var labels = document.getElementById("fname").value;  
+    console.log(labels);
+    var x = this.files[0]['name']
+    fetch(' https://dl2y7ow6n3.execute-api.us-east-1.amazonaws.com/S1/upload/b2photostorage/'+x,{
         method:'PUT',
         headers:{
-            'Content-Type': 'image/png, image/jpg',
-            'x-amz-meta-customLabels': label_input,
-            'x-api-key': 'FsGSZy92TQ2ciUyTD7cbD39eDIzpngEz5yn99XvU'
+            'Content-Type': this.files[0].type,
+          
+            "x-amz-meta-customLabels": labels        
         },
-        body: image_input
-    })
+        body: this.files[0]
+    }) 
+    
 })
+
+
 
 
